af6d66ebfc4a35d2001d0616f74d5d0708b9811b
Matias Jara <mjara.figueroa@gmail.com>
2017-08-30 14:28:55 -0300

    agregando seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# eliminar todos los registros
+Category.destroy_all
+#crear 3 registros en category
+Category.create([{name: 'higiene'},{name: 'abarrotes'},{name: 'aire libre'}])
+# agregar producto con su llave foranea con build
+cat = Category.find_by(name: 'abarrotes')
+cat.products.build(name: 'pasta').save
+# agregar productos con create
+Product.create(name: 'patines', category_id: Category.find_by(name: 'aire libre').id)
+# eliminar ultimo producto
+Product.last.destroy
+# actualizar registro premium a true
+pro = Product.last
+pro.premium = true
+pro.save
+# actualizar todos los registros name a mayusculas
+cat = Category.all
+cat.each do |category|
+    category.name = category.name.upcase
+    category.save
+end
