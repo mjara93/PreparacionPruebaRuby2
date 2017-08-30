64f1f22c0d22fe0cd4289111d4e91be3201dc9a0
Matias Jara <mjara.figueroa@gmail.com>
2017-08-30 15:40:00 -0300

    callbacl y metodos en products

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,4 +4,21 @@ class Product < ApplicationRecord
   scope :premium, ->{where(premium: true)}
   scope :last_n, ->(num) {limit(num)}
 
+  after_destroy :last_product
+
+  def last_product
+    Category.all.each do |category|
+      if category.products.empty?
+        category.destroy
+      end
+    end
+  end
+
+  def precio_final
+    disc = self.category.discount
+    pric = self.price
+
+    descuento = (pric * disc.to_f)/100
+    return pric - descuento
+  end
 end
